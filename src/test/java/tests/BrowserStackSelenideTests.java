fbca10f43d4ebcd656f8f156b2a8d129594ee667
OTretyakova <OTretyakova@beeline.kz>
2021-12-04 21:36:48 +0600

    add verifyLanguageTest

--- src/test/java/tests/BrowserStackSelenideTests.java
+++ src/test/java/tests/BrowserStackSelenideTests.java
@@ -1,11 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
@@ -23,5 +26,20 @@ public class BrowserStackSelenideTests extends TestBase {
         step("Verify content found", () ->
                 $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("Verify language in wikipedia android app")
+    void verifyLanguageTest() {
+        step("Click menu button", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/menu_overflow_button")).click();
+        });
+        step("Click setting button", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/explore_overflow_settings")).click();
+        });
+        step("Click language setting button", () -> {
+            String language = $(MobileBy.xpath("//*[@text='English']")).getAttribute("text");
+            Assertions.assertEquals(language ,"English");
+        });
     }
 }
