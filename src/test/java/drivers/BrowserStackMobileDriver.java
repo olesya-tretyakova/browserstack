f360881bcb7d643b90b406337df726a38287ff05
OTretyakova <OTretyakova@beeline.kz>
2021-12-04 20:06:15 +0600

    Initial commit

---
+++ src/test/java/drivers/BrowserStackMobileDriver.java
@@ -0,0 +1,44 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class BrowserStackMobileDriver implements WebDriverProvider {
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
+
+
+        // Set your access credentials
+        desiredCapabilities.setCapability("browserstack.user", "bsuser_I6EvoW");
+        desiredCapabilities.setCapability("browserstack.key", "d1pLpACFqg7ngcyXp4gq");
+
+        // Set URL of the application under test
+        desiredCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+
+        // Specify device and os_version for testing
+        desiredCapabilities.setCapability("device", "Google Pixel 3");
+        desiredCapabilities.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        desiredCapabilities.setCapability("project", "First Java Project");
+        desiredCapabilities.setCapability("build", "browserstack-build-1");
+        desiredCapabilities.setCapability("name", "first_test");
+
+        return new AndroidDriver(getBrowserstackUrl(),desiredCapabilities);
+    }
+}
---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import static io.restassured.RestAssured.given;
+
+public class Browserstack {
+    public static String videoUrl(String sessionId) {
+        return given()
+                .auth().basic("bsuser_I6EvoW", "d1pLpACFqg7ngcyXp4gq")
+                .when()
+                .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId + ".json")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract()
+                .response()
+                .path("automation_session.video_url");
+
+    }
+}
